aac81cb3c6fe88dd24d12da91ac67267a7f857a2
laodai2035-boop <laodai12035@gmail.com>
2026-07-06 11:04:51 +0700

    Xay dung tang Controller & tao lop Student Controller & khai bao cac phuong thuc tang service trong day

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import model.Student;
+import service.IStudentService;
+import service.StudentService;
+
+import java.util.List;
+
 public class StudentController {
+    IStudentService studentService = new StudentService();
+    public List<Student> getAllStudents(){
+        return studentService.findAll();
+    }
+    public void addStudents(Student student){
+        studentService.add(student);
+    }
+    public void updateStudent (Student student){
+        studentService.update(student);
+    }
+    public void deleteStudent(String msv){
+        studentService.delete(msv);
+    }
+    public List<Student> findStudentsbyName(String name){
+        return studentService.findByName(name);
+    }
 }
